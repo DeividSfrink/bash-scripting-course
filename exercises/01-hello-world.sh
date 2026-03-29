8078ca6b53d56154d2f50e3811c6f034b13743a6
David Garnica Silva <deividsfrink@hotmail.com>
2026-03-29 17:40:13 +0200

    Initial commit: Setup repo structure, exercises folder, and 01-hello-world.sh

---
+++ exercises/01-hello-world.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------
+# Script Name: 01-hello-world.sh
+# Description: Greets the user and displays the current path.
+# ------------------------------------------------------------------
+
+# Print a friendly greeting to the terminal
+echo "Hello World!"
+
+# Display the absolute path of the directory the user is currently in
+echo "My current directory is:"
+pwd
